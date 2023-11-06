5257d5db70526cfbb6ab885305be65f00c653e1d
csilhanek <73843533+csilhanek@users.noreply.github.com>
2023-11-06 15:31:26 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,13 +1,19 @@
-var video = document.querySelector(".video");
+var video = document.querySelector("#player1");
 
 window.addEventListener("load", function() {
 	console.log("Good job opening the window");
+	video.autoplay = false;
+	video.loop = false;
+	video.load();
+	video.pause();
+	video.volume = document.querySelector("#slider").value / 100;
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%"
 });
 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
-	video.load();
 	video.play();
+	// Display volume after play
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
@@ -17,35 +23,42 @@ document.querySelector("#pause").addEventListener("click", function() {
 
 document.querySelector("#slower").addEventListener("click", function() {
 	console.log("Slow Down");
-	video.playbackRate = 0.9;
-	// Slow video down by proper amount
+	var newSpeed = video.playbackRate * 0.9;
+	console.log(newSpeed);
+	video.playbackRate = newSpeed;
 });
 
 document.querySelector("#faster").addEventListener("click", function() {
 	console.log("Speed Up");
-	video.playbackRate = 1.1;
-	// Speed up video by proper amount
+	var newSpeed = video.playbackRate * (10/9);
+	console.log(newSpeed);
+	video.playbackRate = newSpeed;
 });
 
 document.querySelector("#skip").addEventListener("click", function() {
 	console.log("Skip Ahead");
-	// Skip ahead by proper amount
+	video.currentTime += 10;
 });
 
 document.querySelector("#mute").addEventListener("click", function() {
-
+	var mutedVolume = 1;
+	if (video.volume != 0) {
+		mutedVolume = video.volume;
+		video.volume = 0;
+		document.querySelector("#mute").innerHTML = "Unmute";
+	}
+	else {
+		video.volume = mutedVolume;
+		document.querySelector("#mute").innerHTML = "Mute"
+	}
 });
 
 document.querySelector("#vintage").addEventListener("click", function() {
-
+	// Set to oldSchool
 });
 
 document.querySelector("#orig").addEventListener("click", function() {
-
+	// Remove oldSchool
 });
 
-// Display volume after play
-// Fix mute button
 // Fix volume slider
-
-// Old school vs normal style
