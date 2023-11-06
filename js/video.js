9a7781e65f8521a09387e84e370a113349bea6c1
csilhanek <73843533+csilhanek@users.noreply.github.com>
2023-11-06 09:29:42 -0500

    Add files via upload

--- js/video.js
+++ js/video.js
@@ -1,23 +1,28 @@
-var video = document.getElementById("video");
+var video = document.querySelector(".video");
 
 window.addEventListener("load", function() {
-	console.log("Good job opening the window")
+	console.log("Good job opening the window");
 });
 
 document.querySelector("#play").addEventListener("click", function() {
 	console.log("Play Video");
+	video.load();
+	video.play();
 });
 
 document.querySelector("#pause").addEventListener("click", function() {
 	console.log("Pause Video");
+	video.pause();
 });
 
 document.querySelector("#slower").addEventListener("click", function() {
 	console.log("Slow Down");
+	video.playbackRate = 0.5;
 });
 
 document.querySelector("#faster").addEventListener("click", function() {
 	console.log("Speed Up");
+	video.playbackRate = 2;
 });
 
 document.querySelector("#skip").addEventListener("click", function() {
